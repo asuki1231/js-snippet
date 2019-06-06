aa9851115ced42e2ffa34ecb377af7373cf78c5e
kosuge <kosuge19881231@gmail.com>
2019-06-06 23:25:50 +0900

    セミコロン忘れ

--- js-snippet/insertAdjacentElement/index.js
+++ js-snippet/insertAdjacentElement/index.js
@@ -8,21 +8,21 @@
 // afterend:自身の直後に挿入
 
 const bbElm = document.getElementById('beforebegin'),
-      abElm = document.getElementById('afterbegin'),
-      beElm = document.getElementById('beforeend'),
-      aeElm = document.getElementById('afterend'),
-     insertElm1 = document.createElement('span');
-     insertElm1.innerText = ' * insertElm * ';
-     insertElm2 = document.createElement('span');
-     insertElm2.innerText = ' * insertElm * ';
-     insertElm3 = document.createElement('span');
-     insertElm3.innerText = ' * insertElm * ';
-     insertElm4 = document.createElement('span');
-     insertElm4.innerText = ' * insertElm * ';
+    abElm = document.getElementById('afterbegin'),
+    beElm = document.getElementById('beforeend'),
+    aeElm = document.getElementById('afterend'),
+    insertElm1 = document.createElement('span');
+insertElm1.innerText = ' * insertElm * ';
+insertElm2 = document.createElement('span');
+insertElm2.innerText = ' * insertElm * ';
+insertElm3 = document.createElement('span');
+insertElm3.innerText = ' * insertElm * ';
+insertElm4 = document.createElement('span');
+insertElm4.innerText = ' * insertElm * ';
 bbElm.insertAdjacentElement('beforebegin', insertElm1);
-abElm.insertAdjacentElement('afterbegin',insertElm2);
-beElm.insertAdjacentElement('beforeend',insertElm3);
-aeElm.insertAdjacentElement('afterend',insertElm4)
+abElm.insertAdjacentElement('afterbegin', insertElm2);
+beElm.insertAdjacentElement('beforeend', insertElm3);
+aeElm.insertAdjacentElement('afterend', insertElm4);
 
 
 // オプションがキャメルケースじゃないのが不思議だが便利
