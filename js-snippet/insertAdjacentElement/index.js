184812fc0a879184769229047edad3f915f33974
kosuge-takuhiro <kosuge_takuhiro@waku-2.com>
2019-06-06 18:37:06 +0900

    insertAdjacentElement

--- js-snippet/insertAdjacentElement/index.js
+++ js-snippet/insertAdjacentElement/index.js
@@ -7,8 +7,22 @@
 // beforeend: 最後の子ノードとして挿入
 // afterend:自身の直後に挿入
 
-const hoge = document.getElementById('hoge'),
-    fuga = document.createElement('span');
-hoge.insertAdjacentElement('beforebegin', fuga);
+const bbElm = document.getElementById('beforebegin'),
+      abElm = document.getElementById('afterbegin'),
+      beElm = document.getElementById('beforeend'),
+      aeElm = document.getElementById('afterend'),
+     insertElm1 = document.createElement('span');
+     insertElm1.innerText = ' * insertElm * ';
+     insertElm2 = document.createElement('span');
+     insertElm2.innerText = ' * insertElm * ';
+     insertElm3 = document.createElement('span');
+     insertElm3.innerText = ' * insertElm * ';
+     insertElm4 = document.createElement('span');
+     insertElm4.innerText = ' * insertElm * ';
+bbElm.insertAdjacentElement('beforebegin', insertElm1);
+abElm.insertAdjacentElement('afterbegin',insertElm2);
+beElm.insertAdjacentElement('beforeend',insertElm3);
+aeElm.insertAdjacentElement('afterend',insertElm4)
+
 
 // オプションがキャメルケースじゃないのが不思議だが便利
