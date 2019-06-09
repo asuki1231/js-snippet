eabcac094a200c68e120e3a10e5e55633d37ade3
kosuge <kosuge19881231@gmail.com>
2019-06-09 23:23:42 +0900

    progress XMLHttpRequest

---
+++ js-snippet/progress/main.js
@@ -0,0 +1,19 @@
+const req = new XMLHttpRequest();
+req.responseType = 'blob';
+req.addEventListener('progress', (event) => {
+    const rate = event.loaded / event.total;
+    const element = document.querySelector('.progress-bar');
+    element.style.width = `${rate * 100}%`;
+});
+
+req.addEventListener('load', (event) => {
+    const data = event.target.response;
+    const source = URL.createObjectURL(data);
+    const image = new Image();
+    image.src = source;
+    document.querySelector('#log').appendChild(image);
+});
+
+//cross origin
+req.open('GET', 'https://via.placeholder.com/900');
+req.send();
