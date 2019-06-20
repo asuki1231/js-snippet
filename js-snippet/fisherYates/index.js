89dfd6c55c6841c2d491e6b605f96437dc017a21
kosuge <kosuge19881231@gmail.com>
2019-06-20 23:32:04 +0900

    fisherYates

---
+++ js-snippet/fisherYates/index.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5];
+const arrayLength = array.length;
+
+for (let i = array.length - 1; i >= 0; i--) {
+  const randomIndex = Math.floor(Math.random() * (i + 1));
+  [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+}
+
+console.log(array);
