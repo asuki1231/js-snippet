1179b8075a4e185118f20665b9489aba7e2525a9
kosuge <kosuge19881231@gmail.com>
2019-06-21 23:55:58 +0900

    fisherYates コメント追加

--- js-snippet/fisherYates/index.js
+++ js-snippet/fisherYates/index.js
@@ -1,6 +1,7 @@
 const array = [1, 2, 3, 4, 5];
 const arrayLength = array.length;
 
+//fisherYates
 for (let i = array.length - 1; i >= 0; i--) {
   const randomIndex = Math.floor(Math.random() * (i + 1));
   [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
